a82d7a62c70a507c47a9b79bd5936d0e7431f80a
Henning Schmiedehausen <henning@schmiedehausen.org>
2020-09-12 16:28:02 -0700

    fixes for backwards incompatible 3.2.3 changes

--- src/main/java/org/basepom/maven/shade/CollectingManifestResourceTransformer.java
+++ src/main/java/org/basepom/maven/shade/CollectingManifestResourceTransformer.java
@@ -15,6 +15,8 @@ package org.basepom.maven.shade;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.jar.Attributes;
@@ -34,26 +36,64 @@ import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
  * shaded jars. Submitted back to Maven as http://jira.codehaus.org/browse/MSHADE-165.
  */
 public final class CollectingManifestResourceTransformer
-    extends ManifestResourceTransformer
+        extends ManifestResourceTransformer
 {
+    private final List<String> defaultAttributes = Arrays.asList("Export-Package",
+            "Import-Package",
+            "Provide-Capability",
+            "Require-Capability");
 
-    // Configuration
-    @SuppressFBWarnings("UWF_UNWRITTEN_FIELD")
     private String mainClass;
 
-    @SuppressFBWarnings("UWF_UNWRITTEN_FIELD")
-    private Map<String, String> manifestEntries;
+    private Map<String, Object> manifestEntries = Collections.emptyMap();
 
     private boolean collectSections = false;
 
-    // Fields
-    private Manifest manifest;
+    private List<String> additionalAttributes = Collections.emptyList();
+
+    // The root manifest
+    private Manifest rootManifest = null;
+
+    private long time = Long.MIN_VALUE;
+
+    /**
+     * If set, the transformer will collect all sections from jar manifests
+     * and adds them to the main manifest.
+     */
+    public void setCollectSections(boolean collectSections) {
+        this.collectSections = collectSections;
+    }
 
     @Override
-    public boolean canTransformResource( String resource )
+    public void setMainClass( String mainClass )
     {
-        if ( JarFile.MANIFEST_NAME.equalsIgnoreCase( resource ) )
-        {
+        this.mainClass = mainClass;
+    }
+
+    @Override
+    public void setManifestEntries(Map<String, Object> manifestEntries)
+    {
+        if (manifestEntries != null) {
+            this.manifestEntries = manifestEntries;
+        } else {
+            this.manifestEntries = Collections.emptyMap();
+        }
+    }
+
+    @Override
+    public void setAdditionalAttributes(List<String> additionalAttributes)
+    {
+        if (additionalAttributes != null) {
+            this.additionalAttributes = additionalAttributes;
+        } else {
+            this.additionalAttributes = Collections.emptyList();
+        }
+    }
+
+    @Override
+    public boolean canTransformResource(String resource)
+    {
+        if (JarFile.MANIFEST_NAME.equalsIgnoreCase(resource)) {
             return true;
         }
 
@@ -61,33 +101,61 @@ public final class CollectingManifestResourceTransformer
     }
 
     @Override
-    public void processResource( String resource, InputStream is, List<Relocator> relocators )
-        throws IOException
+    public void processResource(String resource, InputStream is, List<Relocator> relocators, long time)
+            throws IOException
     {
+        Manifest loadedManifest = new Manifest(is);
+
+        // Relocate the just loaded manifest
+        if (relocators != null && !relocators.isEmpty()) {
+            final Attributes attributes = loadedManifest.getMainAttributes();
+
+            for (final String attribute : defaultAttributes) {
+                final String attributeValue = attributes.getValue(attribute);
+                if (attributeValue != null) {
+                    String newValue = relocate(attributeValue, relocators);
+                    attributes.putValue(attribute, newValue);
+                }
+            }
+
+            for (final String attribute : additionalAttributes) {
+                final String attributeValue = attributes.getValue(attribute);
+                if (attributeValue != null) {
+                    String newValue = relocate(attributeValue, relocators);
+                    attributes.putValue(attribute, newValue);
+                }
+            }
+        }
+
         // We just want to take the first manifest we come across as that's our project's manifest. This is the behavior
         // now which is situational at best. Right now there is no context passed in with the processing so we cannot
         // tell what artifact is being processed.
-        Manifest loadedManifest = new Manifest( is );
+        if (rootManifest == null) {
+            rootManifest = loadedManifest;
 
-        if ( manifest == null )
-        {
-            manifest = loadedManifest;
+            if (time > this.time) {
+                this.time = time;
+            }
         }
 
-        if ( collectSections && loadedManifest != manifest ) {
-            Map<String, Attributes> sections = manifest.getEntries();
+        // collect additional sections into the root manifest
+        if (collectSections && loadedManifest != rootManifest) {
+            Map<String, Attributes> existingRootSections = rootManifest.getEntries();
 
-            for ( Map.Entry<String, Attributes> manifestSection : loadedManifest.getEntries().entrySet() ) {
+            // loop through the sections of the just loaded manifest
+            for (Map.Entry<String, Attributes> manifestSection : loadedManifest.getEntries().entrySet()) {
                 // Find the section that matches this entry or create a new one.
-                Attributes sectionAttributes = sections.get( manifestSection.getKey() );
-                if ( sectionAttributes == null ) {
-                    sectionAttributes = new Attributes();
-                    sections.put( manifestSection.getKey(), sectionAttributes );
+                Attributes existingRootSectionAttributes = existingRootSections.get(manifestSection.getKey());
+
+                // create a new section if it does not already exist
+                if (existingRootSectionAttributes == null) {
+                    existingRootSectionAttributes = new Attributes();
+                    existingRootSections.put(manifestSection.getKey(), existingRootSectionAttributes);
                 }
 
-                // Loop over all attributes in the section.
-                for( Map.Entry<Object, Object> attributeEntry : manifestSection.getValue().entrySet() ) {
-                    sectionAttributes.put( attributeEntry.getKey(), attributeEntry.getValue() );
+                // Add all attributes from that section to the manifest
+                for (Map.Entry<Object, Object> attributeEntry : manifestSection.getValue().entrySet()) {
+                    existingRootSectionAttributes.put(attributeEntry.getKey(), attributeEntry.getValue());
                 }
             }
         }
@@ -100,31 +168,41 @@ public final class CollectingManifestResourceTransformer
     }
 
     @Override
-    public void modifyOutputStream( JarOutputStream jos )
-        throws IOException
+    public void modifyOutputStream(JarOutputStream jos)
+            throws IOException
     {
         // If we didn't find a manifest, then let's create one.
-        if ( manifest == null )
-        {
-            manifest = new Manifest();
+        if (rootManifest == null) {
+            rootManifest = new Manifest();
         }
 
-        Attributes attributes = manifest.getMainAttributes();
+        Attributes attributes = rootManifest.getMainAttributes();
 
-        if ( mainClass != null )
-        {
-            attributes.put( Attributes.Name.MAIN_CLASS, mainClass );
+        if (mainClass != null && !mainClass.isEmpty()) {
+            attributes.put(Attributes.Name.MAIN_CLASS, mainClass);
         }
 
-        if ( manifestEntries != null )
-        {
-            for ( Map.Entry<String, String> entry : manifestEntries.entrySet() )
-            {
-                attributes.put( new Attributes.Name( entry.getKey() ), entry.getValue() );
+        if (manifestEntries != null) {
+            for (Map.Entry<String, Object> entry : manifestEntries.entrySet()) {
+                attributes.put(new Attributes.Name(entry.getKey()), entry.getValue());
             }
         }
 
-        jos.putNextEntry( new JarEntry( JarFile.MANIFEST_NAME ) );
-        manifest.write( jos );
+        jos.putNextEntry(new JarEntry(JarFile.MANIFEST_NAME));
+        rootManifest.write(jos);
+    }
+
+    private String relocate(String originalValue, List<Relocator> relocators)
+    {
+        String newValue = originalValue;
+        for (Relocator relocator : relocators) {
+            String value;
+            do {
+                value = newValue;
+                newValue = relocator.relocateClass(value);
+            }
+            while (!value.equals(newValue));
+        }
+        return newValue;
     }
 }
